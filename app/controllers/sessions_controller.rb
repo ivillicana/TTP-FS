00edf0f0960fc670555dd49a5b388b50895e095b
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-06 10:20:23 -0400

    create sessions#create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:user][:email].downcase)
+    @user = @user.try(:authenticate, params[:user][:password])
+    return redirect_to new_session_path, notice: "Unable to sign in" unless @user
+
+    session[:user_id] = @user.id
+    redirect_to dashboard_path(@user), notice: "Welcome #{@user.name}!"
   end
 
   def destroy
