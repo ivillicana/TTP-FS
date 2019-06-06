8fbecb0d84c0ebbaed2546f2a6d9acfd9a8d437f
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-06 10:08:42 -0400

    add user params to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
