4729de8a3b3449eede5abd6a9f13eb6c0adec0e4
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-06 12:18:11 -0400

    create #portfolio for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   has_many :companies, through: :transactions
 
   has_secure_password
+
+  def portfolio
+    portfolio_hash = {}
+    self.transactions.each do |t|
+      portfolio_hash[t.company.name] ||= Hash.new(0)
+      portfolio_hash[t.company.name][:shares] += t.shares_quantity
+      portfolio_hash[t.company.name][:value] += (t.shares_quantity * t.price_per_share)
+    end
+    portfolio_hash
+  end
 end
