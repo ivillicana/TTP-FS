3901016ee91f4ce5e3990a2f3a704406ef94d84f
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-06 13:42:03 -0400

    create attempt_transaction method

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,22 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :company
+
+  def self.attempt_transaction(company_hash, user, quantity)
+    transaction_price = (company_hash["latestPrice"] * quantity)
+    return false if user.balance < transaction_price
+
+    company = Company.find_or_create_by(ticker_symbol: company_hash["symbol"], name: company_hash["companyName"])
+    transaction_hash = {
+      user: user,
+      company: company,
+      shares_quantity: quantity,
+      price_per_share: company_hash["latestPrice"],
+      status: "complete"
+    }
+    user.balance -= transaction_price
+    user.save
+    Transaction.create(transaction_hash)
+  end
+
 end
