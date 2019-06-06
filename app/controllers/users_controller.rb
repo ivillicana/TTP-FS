978329bba8697fa6f336150fdafd3d13bae51da1
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-06 10:15:12 -0400

    create dashboards controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       return render :new
     end
     session[:user_id] = @user.id
-    redirect_to profile_path, notice: "Welcome #{@user.name}!"
+    redirect_to dashboard_path, notice: "Welcome #{@user.name}!"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :companies
   resources :sessions, only: [:new, :create, :destroy]
+  get '/dashboard', to: 'dashboards#show', as: 'dashboard'
 end
