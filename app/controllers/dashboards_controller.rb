f0c115d5cc2babfc80ab47feb041a9369deee6b5
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-06 11:38:29 -0400

    add transaction action to dashboards controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,13 @@
 class DashboardsController < ApplicationController
 
   def show
-    
+    @user = current_user
+    @companies = @user.companies
   end
+
+  def transactions
+    @user = current_user
+    @transactions = @user.transactions
+  end
+
 end
