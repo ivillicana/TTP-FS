66b4c241139f7968742977a6e9f704611df25068
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-06 16:52:59 -0400

    add validation for quantity

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,6 +4,7 @@ class TransactionsController < ApplicationController
   def create
     ticker = transaction_params[:ticker_symbol]
     quantity = transaction_params[:shares_quantity].to_f
+    return redirect_to dashboard_path, notice: "Please enter valid values" if !validate_quantity(quantity)
 
     response = Faraday.get("https://api.iextrading.com/1.0/stock/#{ticker}/quote")
     if response.success?
@@ -11,7 +12,7 @@ class TransactionsController < ApplicationController
       Transaction.attempt_transaction(company_hash, current_user, quantity)
       flash[:notice] = "Successful Transaction"
     else
-      flash[:notice] = response.reason_phrase
+      flash[:notice] = "Please enter valid values"
     end
     redirect_to dashboard_path
   end
@@ -20,5 +21,9 @@ class TransactionsController < ApplicationController
 
   def transaction_params
     params.permit(:ticker_symbol, :shares_quantity)
+  end
+
+  def validate_quantity(quantity)
+    quantity % 1 == 0 && quantity > 0 ? true : false
   end
 end
