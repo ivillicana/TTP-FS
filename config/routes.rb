b76821f321e949cac58580dee9035cc376faf4a8
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-06 09:55:42 -0400

    create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "sessions#new"
   resources :users, only: [:new, :create]
   resources :companies
-  resources :sessions, only [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 end
