d64bd05337213c2fd21a7754e7805daa6486012b
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-07 15:49:12 -0400

    create portfolio and portfolio_value methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,23 +5,32 @@ class User < ApplicationRecord
   has_secure_password
 
   def portfolio
-    user_companies = self.companies.uniq.map(&:ticker_symbol).join(",")
+    # Create array of user's companies ticker symbols
+    user_companies = self.companies.uniq.map(&:ticker_symbol)
+    # Make request to IEX API for all user's companies
     response = Faraday.get("https://api.iextrading.com/1.0/stock/market/batch") do |req|
-      req.params['symbols'] = user_companies
+      req.params['symbols'] = user_companies.join(",")
       req.params['types'] = 'quote'
     end
-      
+    companies_hash = JSON.parse(response.body)
+    #Calculate portfolio values
+    portfolio_value(user_companies, companies_hash)
+  end
 
+  def portfolio_value(user_companies, companies_hash)
     portfolio_hash = {}
+    # Create hash with total amount of shares for each user company
     self.transactions.each do |t|
-      portfolio_hash[t.company.name] ||= Hash.new(0)
-      portfolio_hash[t.company.name][:shares] += t.shares_quantity
-      portfolio_hash[t.company.name][:value] += (t.shares_quantity * t.price_per_share)
+      portfolio_hash[t.company.ticker_symbol] ||= Hash.new(0)
+      portfolio_hash[t.company.ticker_symbol][:shares] += t.shares_quantity
+    end
+    # Calculate total value of stock for each user company
+    user_companies.each do |comp_symbol|
+      latest_price = companies_hash[comp_symbol]["quote"]["latestPrice"]
+      shares_quantity =  portfolio_hash[comp_symbol][:shares]
+      portfolio_hash[comp_symbol][:value] = latest_price * shares_quantity
     end
     portfolio_hash
   end
-
-  def portfolio_value
-    
-  end
+  
 end
