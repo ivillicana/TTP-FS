8a1dabcae13d6eb8f16cbe6460fddedc7a7ee3b7
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-10 16:26:48 -0400

    refactor response callback

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -14,17 +14,19 @@ const getStockQuotes = () => {
   let symbols = `?symbols=${stockNames}&types=quote`;
   const IEX_BATCH_URL = 'https://api.iextrading.com/1.0/stock/market/batch';
   $.get(IEX_BATCH_URL + symbols)
-  .then(data => {
-    for (const ticker in data){
-      const companyQuote = data[ticker].quote;
-      const companyNameValue = $(`#${ticker} .portfolio-stock-name, #${ticker} .portfolio-stock-value`);
-      if (companyQuote.latestPrice === companyQuote.open) {
-        companyNameValue.addClass('neutral');
-      } else if (companyQuote.latestPrice >= companyQuote.open){
-        companyNameValue.addClass('overperforming');
-      } else {
-        companyNameValue.addClass('underperforming');
-      }
+  .then(handleIEXQuoteResponse);
+};
+
+const handleIEXQuoteResponse = (data) => {
+  for (const ticker in data){
+    const companyQuote = data[ticker].quote;
+    const companyNameValue = $(`#${ticker} .portfolio-stock-name, #${ticker} .portfolio-stock-value`);
+    if (companyQuote.latestPrice === companyQuote.open) {
+      companyNameValue.addClass('neutral');
+    } else if (companyQuote.latestPrice >= companyQuote.open){
+      companyNameValue.addClass('overperforming');
+    } else {
+      companyNameValue.addClass('underperforming');
     }
-  });
+  }
 };
