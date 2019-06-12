5938b2e7b5322c3588ccc15c9ddcf82ef2829ca8
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-12 17:19:13 -0400

    add helper to render user stock portfolio

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  def render_user_portfolio_stocks(portfolio)
+    if !portfolio[:companies].blank?
+      render partial: "dashboards/portfolio-stocks", locals: {portfolio: portfolio}
+    else
+      render partial: "dashboards/none"
+    end
+  end
 end
