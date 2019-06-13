185456fd0864f5714581152399fd9b963a97729d
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-12 20:53:37 -0400

    create render_user_transactions helper and partial

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,13 @@ module UsersHelper
       render partial: "dashboards/none"
     end
   end
+
+  def render_user_transactions(transactions)
+    if !transactions.blank?
+      render partial: "dashboards/stock-transactions", locals: {transactions: transactions}
+    else
+      render partial: "dashboards/none"
+    end
+  end
+
 end
