cc7a374633a524395a35a752d7cf59a87ca74835
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-13 11:32:31 -0400

    add post route to sell stocks

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'dashboards#show', as: 'dashboard'
   get '/transactions', to: 'dashboards#transactions', as: 'transactions'
   post '/transactions', to: 'transactions#create', as: 'create_transaction'
-  
+  get 'user/stocks/:name', to: 'transactions#display_stock', as: 'display_stock'
+  post 'user/stocks/:name', to: 'transactions#sell_stock', as: 'sell_stock'
 end
