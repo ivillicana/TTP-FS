bc4c830f64b5ac6b2904daa785b475c3fe3313fc
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-13 11:33:57 -0400

    create sell_stock action in transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -18,6 +18,26 @@ class TransactionsController < ApplicationController
     redirect_to dashboard_path
   end
 
+  def display_stock
+    @company = Company.find_by(ticker_symbol: params[:name])
+  end
+
+  def sell_stock
+    user = current_user
+    company = Company.find_by(ticker_symbol: params.permit(:name)[:name])
+    ticker = transaction_params[:ticker_symbol]
+    quantity = transaction_params[:shares_quantity].to_f
+
+    response = Faraday.get("https://api.iextrading.com/1.0/stock/#{ticker}/quote")
+    if response.success?
+      company_hash = JSON.parse(response.body)
+      flash[:notice] = Transaction.attempt_sell(company_hash, current_user, quantity)
+    else
+      flash[:notice] = "Please enter valid values"
+    end
+    redirect_to dashboard_path
+  end
+
   private
 
   def transaction_params
