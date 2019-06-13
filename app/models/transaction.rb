59afc1611783fd74ea047283ea6e6a4073fe719c
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-06-13 11:33:04 -0400

    delete stocks.rb model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -20,4 +20,20 @@ class Transaction < ApplicationRecord
     "Transaction Successful"
   end
 
+  def self.attempt_sell(company_hash, user, quantity)
+    transaction_price = (company_hash["latestPrice"] * quantity)
+    company = Company.find_or_create_by(ticker_symbol: company_hash["symbol"], name: company_hash["companyName"])
+    transaction_hash = {
+      user: user,
+      company: company,
+      shares_quantity: -quantity,
+      price_per_share: -company_hash["latestPrice"],
+      status: "complete"
+    }
+    user.balance += transaction_price
+    user.save
+    Transaction.create(transaction_hash)
+    "Transaction Successful"
+  end
+
 end
